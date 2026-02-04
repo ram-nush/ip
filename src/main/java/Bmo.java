0cc85e3aad4cd09649d88186d4b7fa3d4035dfed
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-04 19:58:35 +0800

    Import Scanner object

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Bmo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm BMO");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
