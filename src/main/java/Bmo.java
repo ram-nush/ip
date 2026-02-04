38f8e061c18829c453d1ce8104f141cea6e51afd
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-04 20:22:47 +0800

    Echo user command

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -9,6 +9,10 @@ public class Bmo {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        String command = scanner.nextLine();
+        System.out.println("____________________________________________________________");
+        System.out.println(command);
+        System.out.println("____________________________________________________________");
 
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
