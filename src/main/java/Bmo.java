f4db6bc872edce03b91e49f4911b303100deeb50
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-04 20:51:24 +0800

    Store tasks

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bmo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm BMO");
@@ -11,8 +14,10 @@ public class Bmo {
 
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
+            tasks.add(command);
+
             System.out.println("____________________________________________________________");
-            System.out.println(command);
+            System.out.println("added: " + command);
             System.out.println("____________________________________________________________\n");
 
             command = scanner.nextLine();
