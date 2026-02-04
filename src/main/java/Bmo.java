9f9790fe546dba5f570b78170cc55b87e98f50a5
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-04 20:34:11 +0800

    Add indentation and horizontal lines

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -7,17 +7,18 @@ public class Bmo {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm BMO");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
 
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
             System.out.println("____________________________________________________________");
             System.out.println(command);
-            System.out.println("____________________________________________________________");
+            System.out.println("____________________________________________________________\n");
 
             command = scanner.nextLine();
         }
 
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
 
