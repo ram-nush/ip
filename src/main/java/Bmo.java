6c6ba94d66f97766fbaf2aacc2d115758d2687cb
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-04 19:34:07 +0800

    Add greeting

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -1,5 +1,11 @@
 public class Bmo {
     public static void main(String[] args) {
-        System.out.println("Hello from BMO");
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm BMO");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
