13a419bf8c22a8b6d16c1dd8cfc333c5a3094984
Subramanian Ramasamy <75689075+ram-nush@users.noreply.github.com>
2026-02-05 00:21:49 +0800

    Handle parameters in commands

--- src/main/java/Bmo.java
+++ src/main/java/Bmo.java
@@ -13,7 +13,9 @@ public class Bmo {
         System.out.println("____________________________________________________________");
         System.out.print("\n");
 
-        String command = scanner.nextLine();
+        String userInput = scanner.nextLine();
+        String[] parameters = userInput.split(" ");
+        String command = parameters[0];
         while (!command.equals("bye")) {
             if (command.equals("list")) {
                 System.out.println("____________________________________________________________");
@@ -23,14 +25,17 @@ public class Bmo {
                 System.out.println("____________________________________________________________");
                 System.out.print("\n");
             } else {
-                tasks.add(command);
+                tasks.add(userInput);
 
                 System.out.println("____________________________________________________________");
-                System.out.println("added: " + command);
+                System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
                 System.out.print("\n");
             }
-            command = scanner.nextLine();
+
+            userInput = scanner.nextLine();
+            parameters = userInput.split(" ");
+            command = parameters[0];
         }
 
         System.out.println("____________________________________________________________");
